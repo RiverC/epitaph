2e6d61a1a20430d8bbbdfa93a7a9eff63e0d273e
JamesThorne <jamesthorne333@gmail.com>
2017-01-31 02:12:09 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mkremins/epitaph "0.0-SNAPSHOT"
+(defproject jamesthorne/epitaph "0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.227"]
                  [org.omcljs/om "0.9.0"]
